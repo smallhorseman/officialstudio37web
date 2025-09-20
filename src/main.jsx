8b35239e9abdc8abd7a487bd3f2af147a2572374
Studio 37 <ceo@ponyboy.win>
2025-09-20 10:45:39 +0000

    d

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 
-// Render the App component into the #root div
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
