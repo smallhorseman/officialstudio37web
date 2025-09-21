b5a8e87afc8924e989196901f7f6898dba1f40eb
Ponyboy <ceo@ponyboy.win>
2025-09-21 06:29:16 -0500

    qd

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,24 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          router: ['react-router-dom']
+        }
+      }
+    }
+  },
+  optimizeDeps: {
+    include: ['react', 'react-dom', 'react-router-dom']
+  },
+  resolve: {
+    alias: {
+      '@': '/src'
+    }
+  }
 });
 
